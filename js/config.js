d10e63532c180d803b69391fe57dd8f4a66285b4
Bhavrattan Singh Bagga <110535685+rattan004@users.noreply.github.com>
2025-04-21 04:39:15 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_PZOTS4aMC', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2tv6lenknrvpnnfqd2lpkh4ov3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_5WKsrI5F9', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'ap-south-1_5WKsrI5F9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
